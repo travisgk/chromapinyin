a5ecc73d726506e9e6fa6ddb550f02be460a85ac
Travis G_____ <162270915+travisgk@users.noreply.github.com>
2024-04-18 11:39:01 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
-from chromapinyin.vowel_chars import (
-	get_tone_num, strip_tone_marker, place_tone_marker
-)
-import chromapinyin.syllables
+import chromapinyin
 
 def main():
 	sample_list = "shén me shí hòu".split()
 	for syllable in sample_list:
-		tone_num = get_tone_num(syllable)
-		stripped = strip_tone_marker(syllable)
-		print(place_tone_marker(stripped, tone_num), end=" ")
+		tone_num = chromapinyin.get_tone_num(syllable)
+		stripped = chromapinyin.strip_tone_marker(syllable)
+		print(chromapinyin.place_tone_marker(stripped, tone_num), end=" ")
 	print("\n")
 
 	hanzi = "老板想买哪种水果。"
 	pinyin = "lǎobǎn xiǎng mǎi nǎ zhǒng shuǐguǒ."
 	inflections = [[3, 3], [3], [3], [3], [3], [3, 3]]
-	syllables = chromapinyin.syllables.create_list(hanzi, pinyin)
+	syllables = chromapinyin.create_syllable_list(hanzi, pinyin)
 	for word in syllables:
 		for syllable in word:
 			print(syllable["hanzi"], end="")
@@ -24,7 +21,7 @@ def main():
 	hanzi = "李老板想找你演讲。"
 	pinyin = "lǐ lǎobǎn xiǎng zhǎo nǐ yǎnjiǎng."
 	inflections = [[3], [3, 3], [3], [3], [3], [3, 3]]
-	syllables = chromapinyin.syllables.create_list(hanzi, pinyin)
+	syllables = chromapinyin.create_syllable_list(hanzi, pinyin)
 	for word in syllables:
 		for syllable in word:
 			print(syllable["hanzi"], end="")
@@ -34,7 +31,7 @@ def main():
 	hanzi = "我比你小。"
 	pinyin = "wǒ bǐ nǐ xiǎo."
 	inflections = [[3], [3], [3], [3]]
-	syllables = chromapinyin.syllables.create_list(hanzi, pinyin)
+	syllables = chromapinyin.create_syllable_list(hanzi, pinyin)
 	for word in syllables:
 		for syllable in word:
 			print(syllable["hanzi"], end="")
@@ -44,7 +41,7 @@ def main():
 	hanzi = "我买与散。"
 	pinyin = "wǒ mǎi yǔsǎn."
 	inflections = [[3], [3], [3, 3]]
-	syllables = chromapinyin.syllables.create_list(hanzi, pinyin)
+	syllables = chromapinyin.create_syllable_list(hanzi, pinyin)
 	for word in syllables:
 		for syllable in word:
 			print(syllable["hanzi"], end="")
@@ -54,7 +51,7 @@ def main():
 	hanzi = "我很好。"
 	pinyin = "wǒ hěn hǎo."
 	inflections = [[3], [3], [3]]
-	syllables = chromapinyin.syllables.create_list(hanzi, pinyin)
+	syllables = chromapinyin.create_syllable_list(hanzi, pinyin)
 	for word in syllables:
 		for syllable in word:
 			print(syllable["hanzi"], end="")
@@ -64,7 +61,7 @@ def main():
 	hanzi = "很勇敢。"
 	pinyin = "hěn yǒnggǎn."
 	inflections = [[3], [3, 3]]
-	syllables = chromapinyin.syllables.create_list(hanzi, pinyin)
+	syllables = chromapinyin.create_syllable_list(hanzi, pinyin)
 	for word in syllables:
 		for syllable in word:
 			print(syllable["hanzi"], end="")
